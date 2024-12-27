3786921abc340b52fd9b91fd4c4eb11f6557d780
rizaldifahmi <fahmirizaldi@live.com>
2024-12-27 10:38:01 +0700

    style: konfigurasi favicon dan microsoft tiles

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,22 @@ import { cn } from '@/lib/utils';
 
 export const metadata: Metadata = seo({
   ...DEFAULT_METADATA,
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [{ url: '/apple-touch-icon.png' }],
+    other: [
+      {
+        rel: 'mask-icon',
+        url: '/safari-pinned-tab.svg',
+        color: '#000000',
+      },
+    ],
+  },
+  manifest: '/site.webmanifest',
 });
 
 export const viewport: Viewport = {
