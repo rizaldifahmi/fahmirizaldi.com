7da7e30b7d647457ada23934e5e3e9f8e549cced
Fahmi Rizaldi <fahmirizaldi@live.com>
2025-07-20 01:34:47 +0800

    fix: update footer text to specify current location in Denpasar, Bali, Indonesia

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -43,7 +43,7 @@ const Footer = () => {
           <div className={cn('font-medium')}>
             &copy; {new Date().getFullYear()}{' '}
             <Link href="/">{SITE.author.name}</Link> ——{' '}
-            <em className={cn('text-muted-foreground')}>Bali, Indonesia</em>
+            <em className={cn('text-muted-foreground')}>currently in Denpasar, Bali, Indonesia</em>
           </div>
           <div className={cn('flex gap-4')}>
             {FOOTER_ICON_LINKS.map(({ title, url, icon, className }, index) => (
