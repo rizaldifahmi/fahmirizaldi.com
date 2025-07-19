2670ebe0f14240d5131e866bc34cc192805f533c
Fahmi Rizaldi <fahmirizaldi@live.com>
2025-07-20 01:33:33 +0800

    fix: update footer location from Bandung to Bali, Indonesia

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -43,7 +43,7 @@ const Footer = () => {
           <div className={cn('font-medium')}>
             &copy; {new Date().getFullYear()}{' '}
             <Link href="/">{SITE.author.name}</Link> ——{' '}
-            <em className={cn('text-muted-foreground')}>Bandung, Indonesia</em>
+            <em className={cn('text-muted-foreground')}>Bali, Indonesia</em>
           </div>
           <div className={cn('flex gap-4')}>
             {FOOTER_ICON_LINKS.map(({ title, url, icon, className }, index) => (
